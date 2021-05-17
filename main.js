2741266bda2f211aeb6ab25ccf834d799a09434a
kaustubh-khandwal <76221706+kaustubh-khandwal@users.noreply.github.com>
2021-05-17 17:57:56 +0530

    updated main
    main file link updated

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function adduser() {
     document.write(window.location.href);
     localStorage.setItem("player1_name", player_1);
     localStorage.setItem("player2_name", player_2);
-    alert("your data has been saved");
-    window.location= "https://github.com/kaustubh-khandwal/ADV-C89/blob/b5a7876b3d8c6129f5da8e9a8e1a0a4522820612/game_page.html";
+    alert("your data has been saved");/
+    window.location= "https://github.com/kaustubh-khandwal/ADV-C89/game_page.html";
 }
 
 //
